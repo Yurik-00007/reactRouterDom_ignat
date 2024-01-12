8a212f22dc3d35d3de805f71e1e2425a5e9ccfb4
Timofeev Yury <yurik-007@mail.ru>
2024-01-13 01:15:27 +0300

    source code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import {Navigate, NavLink, Outlet, Route, Routes, useNavigate, useParams} from "react-router-dom";
-import {realpath} from "fs";
+import {NavLink, Route, Routes} from "react-router-dom";
 
 
+function App() {
+
+    return (
+        <div className="App">
+            <NavLink to={ '/' }>main</NavLink>----
+            <NavLink to={ '/login' }>login</NavLink>---
+            <NavLink to={ '/profile' }>profile</NavLink>---
+
+            <Routes>
+                <Route path={ '/' } element={ <div>main</div> }/>
+                <Route path={ '/login' } element={ <div>login</div> }/>
+                <Route path={ '/profile' } element={ <div>profile</div> }/>
+            </Routes>
+        </div>
+    )
+}
+
+export default App;
+
+
+/*
 const Profile=()=>{
     const navigate=useNavigate()
-/*
-    useEffect(()=>{
-        if(true)navigate('/login')
-    },[])
-    })
-*/
+    /!*
+        useEffect(()=>{
+            if(true)navigate('/login')
+        },[])
+        })
+    *!/
     return <div>
         profile
         <button onClick={()=>{navigate(-1)}}>logout</button>
     </div>
 
 }
+*/
 
+/*
 function App() {
 
     return (
@@ -33,27 +55,25 @@ function App() {
                target={'_blank'}
                // новая вкладка
                rel={'noopener noreferrer nofollow'}>
-                {/*//для безопасности*/}
+                {/!*!//для безопасности*!/}
                 xxxx
             </a>
 
             <Routes>
-                <Route path={ '/*' } element={ <div>404</div> }/>
+                <Route path={ '/!*' } element={ <div>404</div> }/>
                 <Route path={ '/' } element={ <div>main</div> }/>
-                <Route path={ '/login/*' } element={ <div>login</div> }/>
-                {/*<Route path={ '/profile/:x/:y' } element={<Profile/> }/>*/}
-{/*
+                <Route path={ '/login/!*' } element={ <div>login</div> }/>
+                {/!*<Route path={ '/profile/:x/:y' } element={<Profile/> }/>*!/}
+{/!*
                 <Route path={ '/profile/:id' } element={<Profile/> }/>
-*/}
-                <Route path={ '/profile/*' } element={<Profile/> }/>
+*!/}
+                <Route path={ '/profile/!*' } element={<Profile/> }/>
 
             </Routes>
         </div>
     )
 }
-
-export default App;
-
+*/
 
 {/* <Route path={ '/profile/' } element={ (
                     <div>
